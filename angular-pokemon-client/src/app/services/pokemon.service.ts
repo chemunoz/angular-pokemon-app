79105f1d778d0a3d7dc49d4f1ad0450967418bc7
Chema Muñoz <chemunoz78@gmail.com>
2026-04-21 12:05:24 +0200

    refactor: use inject() function in PokemonService

--- angular-pokemon-client/src/app/services/pokemon.service.ts
+++ angular-pokemon-client/src/app/services/pokemon.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Pokemon } from '../models/pokemon.model';
@@ -7,9 +7,8 @@ import { Pokemon } from '../models/pokemon.model';
   providedIn: 'root'
 })
 export class PokemonService {
+  private http = inject(HttpClient);
   private apiUrl = 'http://localhost:8080/api/pokemons';
-
-  constructor(private http: HttpClient) {}
 
   getAllPokemon(): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>(`${this.apiUrl}/`);
@@ -23,11 +22,11 @@ export class PokemonService {
     return this.http.post<Pokemon>(`${this.apiUrl}/custom`, pokemon);
   }
 
-  deleteCustomPokemon(id: string): Observable<any> {
+  deleteCustomPokemon(id: string): Observable<unknown> {
     return this.http.delete(`${this.apiUrl}/custom/${id}`);
   }
 
-  syncFromApi(): Observable<any> {
+  syncFromApi(): Observable<unknown> {
     return this.http.post(`${this.apiUrl}/sync`, {});
   }
 
